3734bb4f03a968275361ccc2da7eddc0db5476dd
+ <+>
2022-03-25 14:05:19 -0400

    commit tree 6e7aeccbff892ab1b98431cd52ac7076cbd3f69d

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //write a function that iterates over each element in the array
+  for (let i = 0; i < array.length; i++) {
+    // find a number that adds up to the target number
+    let sum = target - array[i];
+    // iterate rest of array
+    for (let j = i + 1; j < array.length; j++) {
+      // check to see if numbers add up to the target
+      if (array[j] === sum) 
+        return true
+    }
+  }
+  return false
 }
+// Write your algorithm here
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,7 +20,9 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+*/// function should return true if any 2 numbers in the array adds up to the target number. 
+
+
 
 /*
   Add written explanation of your solution here
@@ -17,7 +31,10 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+console.log(hasTargetSum)
+
   console.log("Expecting: true");
+
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
   console.log("");
